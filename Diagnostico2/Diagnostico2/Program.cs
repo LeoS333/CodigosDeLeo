0e75488ddc3a906eb971bb258818f3f6b6f59a98
LeoS333 <leonardoscarano31@gmail.com>
2025-03-19 21:36:20 -0300

    Punto 4: bucle for de 1 al 50

---
+++ Diagnostico2/Diagnostico2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Diagnostico2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int N = 1; N < 51; N++)
+            {
+                Console.WriteLine("Numero: " + N);
+            }
+            Console.ReadKey();
+        }
+    }
+}
