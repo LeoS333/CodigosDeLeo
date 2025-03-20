727ec0c67ceb64990b0c742a4bf4df5f2f9bd2ea
LeoS333 <leonardoscarano31@gmail.com>
2025-03-19 22:36:40 -0300

    Punto 666

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minimo = 0;
+            Console.WriteLine("Ingrese desde que numero quiere contar");
+            int.Parse(Console.ReadLine());
+            int maximo = 0;
+            Console.WriteLine("Ingrese hasta que numero quiere contar");
+            int.Parse(Console.ReadLine());
+            for (int n = minimo; n < maximo; n++)
+            {
+                Console.WriteLine("Numeros: " + n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
